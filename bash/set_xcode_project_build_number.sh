b2106b53e73d0caaa40fd50a0d543e7c52b8d71d
Viktor Benei <viktor.benei@gmail.com>
2015-04-02 15:03:55 +0200

    Pre-configured bash/set_xcode_project_build_number.sh to handle the case if it's in the same directory as the Info.plist file

--- bash/set_xcode_project_build_number.sh
+++ bash/set_xcode_project_build_number.sh
@@ -3,7 +3,10 @@
 #
 # Don't forget to set the CONFIG_INFO_PLIST_PATH environment
 #  in your Workflow, or replace it with your project's Info.plist path!
+# Or put this script into the same directory which contains your Info.plist file
 #
+
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # exit if a command fails
 set -e
@@ -14,7 +17,12 @@ set -v
 # ---------------------
 # --- Configs:
 
+# [!] Choose one from these!
+# if you want to set the Plist path yourself
 CONFIG_project_info_plist_path="${CONFIG_INFO_PLIST_PATH}"
+# if you put this script into the same directory as the Info.plist file
+CONFIG_project_info_plist_path="${THIS_SCRIPT_DIR}/Info.plist"
+
 CONFIG_new_bundle_version="${BITRISE_BUILD_NUMBER}"
 
 
