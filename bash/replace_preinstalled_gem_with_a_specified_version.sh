229f0a95230d840192fa25b1c489ab4b468e30e8
Viktor Benei <viktor.benei@gmail.com>
2015-05-05 18:02:46 +0200

    Update replace_preinstalled_gem_with_a_specified_version.sh

--- bash/replace_preinstalled_gem_with_a_specified_version.sh
+++ bash/replace_preinstalled_gem_with_a_specified_version.sh
@@ -12,10 +12,14 @@ echo "=> Current CocoaPods version:"
 pod --version
 
 echo "=> Uninstalling CocoaPods..."
+# uninstall --all versions of the gem
 gem uninstall cocoapods -a
 
 echo "=> Install a specific version of CocoaPods..."
+# install the --version you want to
 gem install cocoapods -v 0.36.4
+# pod setup: this is CocoaPods specific, for other gems you probably don't need any setup
+pod setup
 
 echo "=> New CocoaPods version:"
 pod --version
