c5e4a123016483b4c0191a64a1dab6f03ba2699b
Viktor Benei <viktorbenei@bitrise.io>
2023-09-02 18:20:44 +0200

    Change the code so that it it asks for 3 parameters: first parameter is the gradle taskID, then 2 log file paths.
    The gradle task's log should be searched in both files and saved into two separate output files. The output file names should be based on the related log file's name with the gradle task as prefix.

--- gradle-build-cache-debugger/main.go
+++ gradle-build-cache-debugger/main.go
@@ -5,24 +5,25 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
-func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: go run script.go <log_file> <task_id>")
-		os.Exit(1)
-	}
-
-	logFile := os.Args[1]
-	taskID := os.Args[2]
-
+func processLogFile(taskID string, logFile string) {
 	file, err := os.Open(logFile)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		os.Exit(1)
 	}
 	defer file.Close()
+
+	logfileFileName := filepath.Base(logFile)
+	outputFile, err := os.Create("_gradlelog-" + taskID + "_" + logfileFileName)
+	if err != nil {
+		fmt.Println("Error creating output file:", err)
+		os.Exit(1)
+	}
+	defer outputFile.Close()
 
 	reader := bufio.NewReader(file)
 	printTask := false
@@ -40,11 +41,25 @@ func main() {
 		}
 
 		if printTask {
-			fmt.Print(line)
+			fmt.Fprint(outputFile, line)
 		}
 
 		if err == io.EOF {
 			break
 		}
 	}
+}
+
+func main() {
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: go run script.go <task_id> <log_file1> <log_file2>")
+		os.Exit(1)
+	}
+
+	taskID := os.Args[1]
+	logFile1 := os.Args[2]
+	logFile2 := os.Args[3]
+
+	processLogFile(taskID, logFile1)
+	processLogFile(taskID, logFile2)
 }
