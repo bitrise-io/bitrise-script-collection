a33460dc69f21fe1b96840daa4c3c98c838afaa4
Viktor Benei <viktorbenei@bitrise.io>
2024-01-18 15:49:23 +0100

    Update re-trigger-pipeline.sh

--- bitrise-steps/re-trigger-pipeline.sh
+++ bitrise-steps/re-trigger-pipeline.sh
@@ -8,6 +8,7 @@ set -x
 
 # This script will re-trigger the same pipeline it was running in.
 # Useful if you want to keep re-building a pipeline, until it fails or to generate lot of test builds.
+# Source: https://github.com/bitrise-io/bitrise-script-collection/blob/master/bitrise-steps/re-trigger-pipeline.sh
 
 export PIPELINE_TO_TRIGGER="$BITRISEIO_PIPELINE_TITLE"
 curl --fail https://app.bitrise.io/app/$BITRISE_APP_SLUG/build/start.json -L --data '{"build_params":{"branch":"'"$BITRISE_GIT_BRANCH"'","commit_message":"'"$BITRISE_GIT_MESSAGE"'","pipeline_id":"'"$PIPELINE_TO_TRIGGER"'"},"hook_info":{"build_trigger_token":"'"$BUILD_TRIGGER_TOKEN"'","type":"bitrise"},"triggered_by":"curl"}'
